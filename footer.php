70ebaafe184f4ee4e5d72644941d876a533c635b
Mitchel Stahlmann <mitchstahlmann@gmail.com>
2017-08-23 19:34:02 -0500

    Step 1

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+echo "		<footer style="text-align: center; color: white; font-size: 1em; text-shadow: 3px 2px red; letter-spacing: .8em;">
+
+	<a id="b" href="index.html">©</a>
+		<a id="g" href="index.html">2</a>
+			<a id="i" href="index.html"0t</a>
+				<a id="rr" href="index.html">1</a>
+					<a id="ss" href="index.html">7</a>
+
+						<a id="b" href="index.html"><br></br</a>
+		<a id="g" href="index.html">m</a>
+			<a id="i" href="index.html"i</a>
+				<a id="rr" href="index.html">t</a>
+					<a id="ss" href="index.html">c</a>
+						<a id="b" href="index.html">h</a>
+		<a id="g" href="index.html">s</a>
+			<a id="i" href="index.html">t</a>
+				<a id="rr" href="index.html">a</a>
+					<a id="ss" href="index.html">h</a>
+						<a id="b" href="index.html">l</a>
+		<a id="g" href="index.html">m</a>
+			<a id="i" href="index.html">a</a>
+				<a id="rr" href="index.html">n</a>
+					<a id="ss" href="index.html">n</a>
+
+					</footer>";
+					?>
